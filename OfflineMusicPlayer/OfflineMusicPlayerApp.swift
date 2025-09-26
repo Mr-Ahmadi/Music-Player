1c78a5c2297352043fbf467406ea022b45d82082
Ali Ahmadi <mr-ahmadi2004@outlook.com>
2025-09-26 23:48:44 +0330

    Initial Commit

---
+++ OfflineMusicPlayer/OfflineMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfflineMusicPlayerApp.swift
+//  OfflineMusicPlayer
+//
+//  Created by Ali Ahmadi on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct OfflineMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
